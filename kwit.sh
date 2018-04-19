3e509c6e33f85b3034a07518d6f8e617facd789a
LIttleAncientForestKami <Tomasz.Borek@gmail.com>
2018-04-19 18:12:21 +0200

    Jeśli żaden szablon nie pasi wpisz tytuł i treść i kwit zostanie wystawiony tak.
    Peer code reviews are the single biggest thing you can do to improve your code.
        -- Jeff Atwood

--- kwit.sh
+++ kwit.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-if [ $# -ne 3 ]; then
-    echo Usage: $0 GitHub_user_who_owns_the_repo repo_name JSON_with_params
+if [[ $# -ne 3 ]] && [[ $# -ne 4 ]]; then
+    echo Usage:
+    echo -ne "\t $0 GitHub_user_who_owns_the_repo repo_name JSON_with_params"
+    echo "or, in case when no template matches what you're after"
+    echo -ne "\t $0 GitHub_user_who_owns_the_repo repo_name issue_title issue_body"
     exit 1
 fi
 . exportToken.sh
@@ -11,6 +14,15 @@ LAFK="LIttleAncientForestKami"
 
 WHOSE=$1
 REPO=$2
-JSON=$3
+if (( $# == 3 )); then
+    JSON=$3
+    curl -u "$LAFK":"$TOKEN" https://api.github.com/repos/"$WHOSE"/"$REPO"/issues -d @"$JSON"
+fi
 
-curl -u "$LAFK":"$TOKEN" https://api.github.com/repos/$WHOSE/$REPO/issues -d @$JSON
+if (( $# == 4 )); then
+    TITLE=$3
+    BODY=$4
+    jq -n --arg t "$TITLE" --arg b "$BODY" -f kwity/szablon.json > json
+    curl -u "$LAFK":"$TOKEN" https://api.github.com/repos/"$WHOSE"/"$REPO"/issues -d @json
+    rm json
+fi
