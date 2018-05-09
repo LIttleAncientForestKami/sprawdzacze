488c286aa1bfa76791b5f355d36677f5ea3eec38
LIttleAncientForestKami <Tomasz.Borek@gmail.com>
2018-05-09 06:10:13 +0200

    Punktacja niemal dobrze Modularyzacja rozwiązaniem.
    Miast szukać gdzie dokładnie który parametr znika lub jest nie tak
    przekazywany pomiędzy trzema funkcjami, przerzucam odpowiedzialność za
    punktację czy opis całości do osobnych funkcji.
    
    Jest lepiej, lecz nie idealnie.
    
    It is easier to port a shell than a shell script.
        -- Larry Wall.

--- reckaZKomciami.sh
+++ reckaZKomciami.sh
@@ -5,9 +5,10 @@ NR=$1
 . exportToken.sh
 LAFK="LIttleAncientForestKami"
 APPv3="Accept: application/vnd.github.full+json"
+LOG=przebieg.log
 
 ghcurl() {
-    curl -f -u "$LAFK":"$TOKEN" "$1" -H "$APPv3"
+    curl -f -# -u "$LAFK":"$TOKEN" "$1" -H "$APPv3"
 }
 
 komcie-do-csv() {
@@ -23,23 +24,29 @@ uwagi-do-recki() {
     jq -r -c '.[] | ["u", .user.login, .body, .created_at, .html_url] | @csv' < uwagi"$NR".json >> komcie"$NR".csv
     cat < 1wszaLiniaKomci - komcie"$NR".csv | sponge komcie"$NR".csv
 }
-## Wyciągnij recki
+
+
+# for tutaj
+touch $LOG
+date >> $LOG
+echo Wyciągnij reckę nr "$NR" >> $LOG
 
 ghcurl https://api.github.com/repos/szczepanskikrs/Battleships/pulls/"$NR" > recka"$NR".json
 touch komcie"$NR".json
-
-## Wyciągnij komcie jak jest co
 
 IL_KOMCI=$(jq .review_comments < recka"$NR".json)
 IL_UWAG=$(jq .comments < recka"$NR".json)
 
 if [ "$IL_KOMCI" -gt 0 ]
 then
-    echo Recka ma komcie, ciągniemy.
+    echo Recka ma komcie, ciągniemy. >> $LOG
     komcie-do-csv
 elif [ "$IL_UWAG" -gt 0 ]
 then
-    echo Komcie są nie tylko w recce! "$IL_KOMCI", "$IL_UWAG"
+    echo Komcie są nie tylko w recce! "$IL_KOMCI", "$IL_UWAG" >> $LOG
     uwagi-do-recki
+else
+    echo SPRAWDŹ! Pusta recka nr "$NR" >> $LOG
 fi
-echo Recka "$NR", uwag "$IL_UWAG", komci "$IL_KOMCI"
+
+echo Recka "$NR", uwag "$IL_UWAG", komci "$IL_KOMCI" >> $LOG
