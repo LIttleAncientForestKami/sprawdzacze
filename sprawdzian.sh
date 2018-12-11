5e5f8fd0ec1e55655d11df2d010c2d27b752562e
Tomasz "LAFK_pl" Borek <Tomasz.Borek@gmail.com>
2018-12-11 21:17:34 +0100

    Mocniejszy i elegantszy skrypt, mniej wywrotny Zastosowano szereg dobrych praktyk:
    Zmienne w cudzysłowiach,
    walidacja nazwy (teraz nazwa jako parametr!),
     czy podana,
     czy istnieje takie coś,
     pętla po zmiennej, nie po sztywnym wpisie
    plik raport.adoc ma pełną ścieżkę (użyto basename),
    cd -> cd | exit,
    kody błędów opisane (1 dla cd, 2 dla nazwy),
    funkcja spr_gi dla walidacji gi by było mniej przekierowań,
    poprawiono sprawdzanie liczb via >, które jest dla napisów
    przy find -iname dodano "" by uniknąć nadinterpretacji powłoki
    
    Trying to outsmart a compiler defeats much of the purpose of using one.
        -- Kernighan & Plauger, The Elements of Programming Style.

--- sprawdzian.sh
+++ sprawdzian.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# exit 1 - cd zawiodło
+# exit 2 - podano zły parametr nazwa lub go nie podano
+
+NAZWA=$1
 
 function porażka() {
     echo "# tag::powód[]" > porażka.adoc
@@ -6,17 +10,36 @@ function porażka() {
     echo "# end::powód[]" >> porażka.adoc
 }
 
-for f in *_NAZWA_TUTAJ; do
-    echo $f
-    cp raport.adoc $f
-    cd $f
+function spr_gi() {
+    echo "Jest .gitignore"
+    echo "Śmieci w repo?"
+    git ls-files | grep -e 'class\target\iml\jar'
+    echo "Jeśli pomiędzy pytaniem a tą linią nic nie ma... to nie ma"
+}
+
+function brak_nazwy_wychodzę() {
+    if [[ "" = "$NAZWA" ]]; then
+        echo "Niezbędny parametr do działania to katalog lub regex katalogowy, np: Jan_OX albo *_OX"
+        exit 2
+    else
+        echo "Sprawdzam istnienie $NAZWA"
+    fi
+    if [[ ! -a "$NAZWA" ]]; then
+        echo "Nie ma katalogu/ów $NAZWA"
+        exit 2
+    fi
+}
+
+brak_nazwy_wychodzę
+
+for f in "$NAZWA"; do
+    echo "Zaczynam pracę nad $f"
+    cp "$(basename "$0")"/raport.adoc "$f"
+    cd "$f" || exit 1
     porażka "Twój program na szczęście nie poniósł takowej."
     git shortlog -sn > shortlog.txt
     if [[ -f .gitignore ]]; then
-        echo "Jest .gitignore" > gi.txt
-        echo "Śmieci w repo?" >> gi.txt
-        git ls-files | grep -e 'class\target\iml\jar' >> gi.txt
-        echo "Jeśli pomiędzy pytaniem a tą linią nic nie ma... to nie ma" >> gi.txt
+        spr_gi > gi.txt
     else 
         echo "Brak .gitignore - zdecydowanie oznaka złej higieny projektu i słabych umiejętności autorki/a" > gi.txt 
     fi
@@ -24,14 +47,14 @@ for f in *_NAZWA_TUTAJ; do
         echo Jest POM
     else 
         porażka "Brak Mavena, nie mam POMa, nie mogę przeprowadzić audytu."
-        cd ..
+        cd .. || exit 1
         continue
     fi
     echo mvnci
-    mvn clean install | egrep Compiling\|resourceDirectory\|Building\|Installing\|"Time elapsed:"\|WARNING\|ERROR\|BUILD\|Total > mvn.log
-    if [[ "$?" > 0 ]]; then
+    mvn clean install | grep -e Compiling\|resourceDirectory\|Building\|Installing\|"Time elapsed:"\|WARNING\|ERROR\|BUILD\|Total > mvn.log
+    if [[ "$?" -gt 0 ]]; then
         porażka "podczas budowy wystąpiły błędy. Spójrz na zapis z budowy."
-        cd ..
+        cd .. || exit 1
         continue
     fi
     echo Mapa pakietów
@@ -41,8 +64,8 @@ for f in *_NAZWA_TUTAJ; do
         if [[ "$d" = "summary.dot" ]]; then
             continue
         fi
-        sfdp -Grotate=90 -Tpng $d -o sfdp-mp.png
-        osage -Tpng $d -o osage-mp.png
+        sfdp -Grotate=90 -Tpng "$d" -o sfdp-mp.png
+        osage -Tpng "$d" -o osage-mp.png
     done
     echo Mapa klas
     jdeps -apionly -verbose:class -R -e "$BAZA"'.*' -dotoutput . target/*.jar
@@ -50,12 +73,12 @@ for f in *_NAZWA_TUTAJ; do
         if [[ "$d" = "summary.dot" ]]; then
             continue
         fi
-        sfdp -Grotate=90 -Tpng $d -o sfdp-mc.png
-        osage -Tpng $d -o osage-mc.png
+        sfdp -Grotate=90 -Tpng "$d" -o sfdp-mc.png
+        osage -Tpng "$d" -o osage-mc.png
     done
-    cd src/main/java
-    find . -iname *.java > ../../../klasy.log
-    cd -
+    cd src/main/java || exit 1
+    find . -iname "*.java" > ../../../klasy.log
+    cd - || exit 1
     asciidoctor-pdf raport.adoc
-    cd ..
+    cd .. || exit 1
 done
