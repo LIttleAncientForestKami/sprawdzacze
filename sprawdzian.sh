6d15476a64e9523788fc31402954b0b019eb0058
Tomasz "LAFK_pl" Borek <Tomasz.Borek@gmail.com>
2021-10-16 16:45:01 +0200

    Przeróbka na sprawdzanie jednego repo, ale lepiej
    1. Warunkowe dyrektywy ADOC by raport zawierał tylko potrzebne fragmenty
    2. Skrypt pracuje z JEDNYM katalogiem, nie z wieloma, `for` skasowany
    3. Tymczasowo utracone: obrazy zależności klas/pakietów, raport z Mavena
    4. Ujednolicono tymczasowe pliki wprowadzając konwencję nazewniczą
    `spr_nazwa.tmplog`, konfigurowalną przez zmienne w skrypcie
    5. Dodano kolory: notka na niebiesko, uwaga na czerwono na białym
    6. Ujednolicono wyjścia ze skryptu przez funkcję `porażka`
    7. Spis treści w raporcie jest zautomatyzowany i dostosowany do
    zawartości - współgra z punktem 1.
    8. Podany parametr musi być katalogiem - dostosowano sprawdzenie
    9. Mniej żonglerki z `cd` w skrypcie - klasy pobierane z
    `src/main/java`, większość cd || exit niepotrzebna skoro praca z 1
    katalogiem (i jednym projektem)
    10. Raport ma nazwę dostosowaną do katalogu, nie jest to zawsze "Audyt
    projektu OX" ale "Audyt projektu nazwa_jaką_podano_skryptowi"
    11. Finałowy raport ma nazwę raport.pdf (może lepsza w przyszłości?)
    12. Brak Mavena powoduje szybki koniec pracy i odpowiedni raport
    13. Poprawa kopiowania raportu do właściwego katalogu
    14. Lepsza dyrektywa Mavena:
    14.1. logowanie do pliku przez -l nazwaPliku
    14.2. wydruki z testów znikają z konsoli bo niepotrzebne
    `-Dmaven.test.redirectTestOutputToFile=true`
    14.3. logowanie od poziomu WARN
    `-Dorg.slf4j.simpleLogger.defaultLogLevel=warn`
    15. Wypis ulepszonego zapisu z budowy na konsolę na razie
    16. Opakowano w funkcje: jest_pom i budujemy (Maven, oba)
    
    Kilka detali (np. językowych) pominięto.
    
    The more I C, the less I see.

--- sprawdzian.sh
+++ sprawdzian.sh
@@ -1,13 +1,52 @@
 #!/bin/bash
 # exit 1 - cd zawiodło
 # exit 2 - podano zły parametr nazwa lub go nie podano
+# exit 3 - audyt zakończony porażką: nie powiodły się potrzebne sprawdzenia i kończymy raportowanie
 
-NAZWA=$1
+# błędy kompilacji i nie przechodzące testy == porażka
+# ostrzeżenia - tylko ujemne punkty
+# raport z błędami w obu wypadkach
+# TODO: przerobić na skrypt działający na jednym katalogu
+# sprzątanie po sobie?
+
+# pliki tymczasowe nazywają się
+PREFIX="spr"
+EXT="tmplog"
+GIG="$PREFIX"_gi."$EXT"
+GIS="$PREFIX"_giShortLog."$EXT"
+GIL="$PREFIX"_giLog."$EXT"
+MVN="$PREFIX"_mvn."$EXT"
+KLA="$PREFIX"_klasy."$EXT"
+set -x
+
+function notka() { echo "$(tput setaf 4)$0: $*$(tput sgr0)" >&2; }
+function uwaga() { echo "$(tput setab 7)$(tput setaf 1)$(tput bold)$0: $*$(tput sgr0)" >&2; }
+
+function brak_nazwy_wychodzę() {
+    if [[ "" = "$NAZWA" ]]; then
+        uwaga "Niezbędny parametr do działania to katalog np: Jan_OX"
+        exit 2
+    else
+        notka "Sprawdzam istnienie $NAZWA"
+    fi
+    if [[ ! -d "$NAZWA" ]]; then
+        uwaga "Nie ma katalogu $NAZWA"
+        exit 2
+    fi
+}
 
 function porażka() {
-    echo "# tag::powód[]" > porażka.adoc
-    echo "Porażka! $1" >> porażka.adoc
-    echo "# end::powód[]" >> porażka.adoc
+    sed -i "s/:a: ATRYBUTY_TUTAJ/:porażka: $1/" $RAPORT
+    asciidoctor-pdf $RAPORT
+    exit 3
+}
+
+function jest_pom() {
+    if [[ -f pom.xml ]]; then
+        notka Jest POM
+    else 
+        porażka "Brak Mavena, nie mam POMa, nie mogę przeprowadzić audytu."
+    fi
 }
 
 function spr_gi() {
@@ -17,68 +56,68 @@ function spr_gi() {
     echo "Jeśli pomiędzy pytaniem a tą linią nic nie ma... to nie ma"
 }
 
-function brak_nazwy_wychodzę() {
-    if [[ "" = "$NAZWA" ]]; then
-        echo "Niezbędny parametr do działania to katalog lub regex katalogowy, np: Jan_OX albo *_OX"
-        exit 2
-    else
-        echo "Sprawdzam istnienie $NAZWA"
-    fi
-    if [[ ! -a "$NAZWA" ]]; then
-        echo "Nie ma katalogu/ów $NAZWA"
-        exit 2
+function budujemy() {
+    notka mvnci, bez logów, także tych z testów
+    mvn -l $MVN clean install -Dmaven.test.redirectTestOutputToFile=true
+    if [[ "$?" -gt 0 ]]; then
+        uwaga """.Błędy kompilacji:
+        ----
+        $(cat $MVN)
+        ----
+        """
+        porażka "podczas budowy wystąpiły błędy. Spójrz na zapis z budowy."
     fi
 }
 
+
+NAZWA=$1
 brak_nazwy_wychodzę
 
-for f in "$NAZWA"; do
-    echo "Zaczynam pracę nad $f"
-    cp "$(basename "$0")"/raport.adoc "$f"
-    cd "$f" || exit 1
-    porażka "Twój program na szczęście nie poniósł takowej."
-    git shortlog -sn > shortlog.txt
-    if [[ -f .gitignore ]]; then
-        spr_gi > gi.txt
-    else 
-        echo "Brak .gitignore - zdecydowanie oznaka złej higieny projektu i słabych umiejętności autorki/a" > gi.txt 
-    fi
-    if [[ -f pom.xml ]]; then
-        echo Jest POM
-    else 
-        porażka "Brak Mavena, nie mam POMa, nie mogę przeprowadzić audytu."
-        cd .. || exit 1
-        continue
-    fi
-    echo mvnci
-    mvn clean install | grep -e Compiling\|resourceDirectory\|Building\|Installing\|"Time elapsed:"\|WARNING\|ERROR\|BUILD\|Total > mvn.log
-    if [[ "$?" -gt 0 ]]; then
-        porażka "podczas budowy wystąpiły błędy. Spójrz na zapis z budowy."
-        cd .. || exit 1
-        continue
-    fi
-    echo Mapa pakietów
-    BAZA=$(ls src/main/java)
-    jdeps -apionly -R -e "$BAZA"'.*' -dotoutput . target/*.jar
-    for d in *.dot; do
-        if [[ "$d" = "summary.dot" ]]; then
-            continue
-        fi
-        sfdp -Grotate=90 -Tpng "$d" -o sfdp-mp.png
-        osage -Tpng "$d" -o osage-mp.png
-    done
-    echo Mapa klas
-    jdeps -apionly -verbose:class -R -e "$BAZA"'.*' -dotoutput . target/*.jar
-    for d in *.dot; do
-        if [[ "$d" = "summary.dot" ]]; then
-            continue
-        fi
-        sfdp -Grotate=90 -Tpng "$d" -o sfdp-mc.png
-        osage -Tpng "$d" -o osage-mc.png
-    done
-    cd src/main/java || exit 1
-    find . -iname "*.java" > ../../../klasy.log
-    cd - || exit 1
-    asciidoctor-pdf raport.adoc
-    cd .. || exit 1
-done
+notka "Zaczynam pracę nad $NAZWA"
+
+RAPORT="$PREFIX"_$(basename $NAZWA)_raport.adoc
+cp "$(dirname "$0")"/raport.adoc "$NAZWA"/"$RAPORT"
+cd "$NAZWA" || exit 1
+sed -i "s/NAZWA_TUTAJ/$(basename $NAZWA)/" $RAPORT
+
+jest_pom
+
+budujemy
+
+notka "Zobaczmy kto nad tym pracował?"
+git shortlog -sn > $GIS
+
+notka "Ignorowane pliki Gita"
+if [[ -f .gitignore ]]; then
+    spr_gi > $GIG
+else 
+    echo "Brak .gitignore - zdecydowanie oznaka złej higieny projektu" > $GIG
+fi
+#echo Mapa pakietów
+#BAZA=$(ls src/main/java)
+#jdeps -apionly -R -e "$BAZA"'.*' -dotoutput . target/*.jar
+#for d in *.dot; do
+#    if [[ "$d" = "summary.dot" ]]; then
+#        exit 3
+#    fi
+#    sfdp -Grotate=90 -Tpng "$d" -o sfdp-mp.png
+#    osage -Tpng "$d" -o osage-mp.png
+#done
+#echo Mapa klas
+#jdeps -apionly -verbose:class -R -e "$BAZA"'.*' -dotoutput . target/*.jar
+#for d in *.dot; do
+#    if [[ "$d" = "summary.dot" ]]; then
+#        exit 3
+#    fi
+#    sfdp -Grotate=90 -Tpng "$d" -o sfdp-mc.png
+#    osage -Tpng "$d" -o osage-mc.png
+#done
+#cd src/main/java || exit 1
+notka Ile klas tu mamy?
+find src/main/java -iname "*.java" > $KLA #../../../klasy.log
+notka "Raport do PDFa i sprzątanie"
+asciidoctor-pdf $RAPORT -o raport.pdf
+#rm spr_*
+notka Koniec pracy
+mupdf raport.pdf
+cd - || exit 1
